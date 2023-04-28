5e07a3126551f2c09c6e9559f556df6ed0656118
hubert-poldevs <hubert@poldevs.com>
2023-04-28 11:12:25 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Start</div>;
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:3001/api/posts");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  return (
+    <div className="App">
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
